f57fd4b2f735d39dc02245b0fc636f23e53e1f1e
Zarrux <165376803+Zarrux0@users.noreply.github.com>
2024-05-22 02:03:54 +0500

    Add files via upload

---
+++ app..js
@@ -0,0 +1,123 @@
+// let sinf= +prompt ("sinfingizni kiriting")
+// switch (sinf){
+//   case 
+// // }
+// // to ..............
+
+// let a = 5;
+// switch (a) {
+//   case 5:
+//     alert=("karea");
+//     break;
+//   case 6:
+//     alert=("uk");
+//   break;
+// }
+
+
+// let a = 10;
+// switch (a) {
+//   case 5:
+//     alert("Kariya");
+//     break;
+// //   case 6:
+// //     alert("Polsha");
+// //     break;
+// //   case 7:
+// //     alert("Kanada");
+// //     break;
+// //   case 8:
+// //     alert("UK");
+// //     break;
+// //   case 9:
+// //     alert("United state");
+// //     break;
+// //   default:
+// //     alert("Bebahosan Marsga bor");
+// // }
+
+// let days= +prompt ("weekend")
+// switch (days) {
+
+//   case 1:
+//     alert ("dushanba")
+//     break;
+//   case 2:
+//     alert ("seshanba")
+//     break
+//   case 3:
+//     alert ("chorshamba")
+//     break
+//   case 4:
+//     alert ("payshamba")
+//     break
+//   case 5:
+//     alert ("juma")
+//     break
+//   case 6:
+//     alert ("shamba")
+//     break
+//   case 7:
+//     alert("yakshamba")
+//     break
+// }
+
+
+
+// let baholar= +prompt ("baholar")
+// switch (baholar) {
+//   case 1:
+//     alert ("yomon")
+//     break
+//     case 2:
+//       alert ("yomon")
+//       break
+//       case 3:
+//         alert("qoniqarsiz")
+//         break 
+//         case 4:
+//           alert("qoniqarli")
+//           break
+//           case 5:
+//             alert("alo")
+//             break  
+//             default:
+//               alert("bunaqa baho yoq")
+//               break 
+// }
+
+
+
+
+
+let a = +prompt("a");
+let b = +prompt("b"); 
+switch (a) {
+  case 1:
+    alert(b / a);
+    break;
+  case 2:
+    alert(b * a);
+    break;
+  case 3:
+    alert(b - a);
+    break;
+  case 4:
+    alert(b / a);
+    break;
+}
+switch (b) {
+    case 5:
+        alert(b / a);
+        break;
+      case 6:
+        alert(b * a);
+        break;
+      case 7:
+        alert(b - a);
+        break;
+      case 8:
+        alert(b / a);
+        break;
+    }
+
